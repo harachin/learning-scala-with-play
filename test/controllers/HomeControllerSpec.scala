6cb6001f00952297c9fc42570c2480afeac993ba
harachin <harachin.dev@gmail.com>
2025-03-25 02:50:45 +0900

    build: cleanup frontend assets and add unit test codes.

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,45 +1,29 @@
-package controllers
+package com.harachin.controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json.Json
+import com.harachin.controllers.HomeController
+import play.test.WithApplication
+import play.inject.guice.GuiceApplicationBuilder
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
-  "HomeController GET" should {
+  "a root" when {
+    "called nomally" should {
+      "be return JSON with data and errors" in {
+        val controller = new HomeController(stubControllerComponents())
+        val result = controller.root().apply(FakeRequest(GET, "/"))
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+        status(result) mustBe OK
+        contentType(result) mustBe Some("application/json")
+        contentAsJson(result) mustBe Json.obj(
+          "data" -> Json.obj("message" -> "Welcome to Learning Play Framework with Scala application."), 
+          "errors" -> Json.arr()
+        )
+      }
     }
   }
 }
