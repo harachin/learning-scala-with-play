0f10541e25f95da13cc8d13f4cd86b40f018344f
harachin <harachin.dev@gmail.com>
2025-03-28 23:03:51 +0900

    ci: add scalafmt.

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -1,25 +1,28 @@
 package com.harachin.controllers
 
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.BaseController as PlayBaseController
 import play.api.mvc.ControllerComponents
 import play.api.mvc.Result
-import play.api.libs.json._
 
 trait BaseController extends PlayBaseController {
-  /**
-    * Format the response in JSON format like:
+
+  /** Format the response in JSON format like:
     * ```json
     * {
     *   "data": {},
     *   "errors" : []
     * }
     * ```
-    * 
-    * @param data JsObject
-    * @param errors List[String]
-    * @return JsObject
+    *
+    * @param data
+    * @param errors
+    * @return
     */
-  def formatAsJson(data: JsObject, errors: List[String] = List()): JsObject = {
+  def formatAsJson(
+    data: JsObject,
+    errors: List[String] = List()
+  ): JsObject = {
     Json.obj("data" -> data, "errors" -> errors)
   }
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,12 +1,13 @@
 package com.harachin.controllers
 
-import javax.inject._
-import play.api.mvc._
-import play.api.libs.json.Json
 import com.harachin.controllers.BaseController
 
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents, Request}
+
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
 
   def root() = Action { implicit request: Request[AnyContent] =>
     Ok(formatAsJson(Json.obj("message" -> "Welcome to Learning Play Framework with Scala application.")))
